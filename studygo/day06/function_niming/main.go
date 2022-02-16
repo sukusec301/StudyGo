2d827cc4dfc809cffe5d5324ac9bed04a3db0ec0
SomaVicago <745429385@qq.com>
2022-02-16 21:48:36 +0800

    1

--- studygo/day06/function_niming/main.go
+++ studygo/day06/function_niming/main.go
@@ -9,4 +9,19 @@ var a = func() {
 
 func main() {
 	a()
+	//函数内部调用匿名函数，相当于用一个变量声明匿名函数，随后调用
+	f1 := func() {
+		fmt.Println("helkad")
+	}
+	f1()
+	//如果只是调用一次，可以简写称立即执行函数
+	func() {
+		fmt.Println("立即执行匿名函数")
+	}() //这里就是立即执行
+
+	//立即调用带有参数的匿名函数
+	func(x int, y int) {
+		ret := x + y
+		fmt.Println(ret)
+	}(10, 20)
 }
